dd0b32c83cb0f7e5d270040dc665fce41bc575bf
Miracle287 <37232645+Miracle287@users.noreply.github.com>
2018-06-17 16:13:33 +0800

    new version 2018.06.17

--- WebContent/assets/js/data_caches.js
+++ WebContent/assets/js/data_caches.js
@@ -1,7 +1,13 @@
 /**
+ * server_url 服务器主地址配置
+ * 桌面版使用远程地址 http://lzy25.s1.natapp.cc/attendentServer/
+ * Web端使用本地服务器地址： http://localhost:8080/attendentServer/
+ */
+//var server_url = "http://localhost:8080/attendentServer/";
+var server_url = "";   //相对地址
+/**
  * 全局变量保存
  */
-var server_url = "testserver";
 var Cache = {
 	type : 'local',
 	setType : function() {
